0e5c1ba96f5e5148870193a9b9959f8aee902e45
Emengkeng <blessedsonlove@gmail.com>
2025-12-08 12:09:22 +0100

    feat: proper type and error system for the project

---
+++ src/errors.ts
@@ -0,0 +1,31 @@
+export class EventopError extends Error {
+  constructor(
+    message: string,
+    public statusCode?: number,
+    public code?: string,
+  ) {
+    super(message);
+    this.name = 'EventopError';
+  }
+}
+
+export class AuthenticationError extends EventopError {
+  constructor(message: string = 'Invalid API key') {
+    super(message, 401, 'authentication_error');
+    this.name = 'AuthenticationError';
+  }
+}
+
+export class InvalidRequestError extends EventopError {
+  constructor(message: string) {
+    super(message, 400, 'invalid_request');
+    this.name = 'InvalidRequestError';
+  }
+}
+
+export class NotFoundError extends EventopError {
+  constructor(message: string) {
+    super(message, 404, 'not_found');
+    this.name = 'NotFoundError';
+  }
+}
